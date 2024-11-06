754002b31976fa28e2f90c6a51271a71f27453d8
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-11-06 11:52:14 +0300

    .

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Кастомный middleware для имитации ошибок
+
 server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
 
-// Имитируем ошибку 500 на эндпоинт /satellites/status в 10% случаев
 server.get('/satellites/status', (req, res) => {
   if (Math.random() < 0.1) {
     res.status(500).jsonp({
       error: "Внутренняя ошибка сервера",
     });
   } else {
-    // Получение статистики по статусам спутников
     const satellites = router.db.get('satellites').value();
     const status = satellites.reduce(
       (acc, sat) => {
@@ -31,7 +28,6 @@ server.get('/satellites/status', (req, res) => {
   }
 });
 
-// Обработка других маршрутов
 server.use(router);
 
 const PORT = 3001;
