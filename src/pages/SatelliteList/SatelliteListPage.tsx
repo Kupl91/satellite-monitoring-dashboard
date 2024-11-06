4682433d3e828daf5adda4a8b48830506b835657
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-11-06 14:22:35 +0300

    + кнопка случайного измения координат спутника и мемо

--- src/pages/SatelliteList/SatelliteListPage.tsx
+++ src/pages/SatelliteList/SatelliteListPage.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import SatelliteList from '../../components/SatelliteList/SatelliteList';
+import React, { useCallback } from 'react';
 import SatelliteStatus from '../../components/SatelliteStatus/SatelliteStatus';
 import SatelliteMap from '../../components/SatelliteMap/SatelliteMap';
+import SatelliteList from '../../components/SatelliteList/SatelliteList';
+import { useGetSatellitesQuery, useUpdateSatelliteMutation, randomizeSatelliteData } from '../../services/satellitesApi';
 import './SatelliteListPage.css';
 
 const SatelliteListPage: React.FC = () => {
+  const { data: satellites } = useGetSatellitesQuery();
+  const [updateSatellite] = useUpdateSatelliteMutation();
+
+  const handleRandomize = useCallback(() => {
+    if (satellites) {
+      randomizeSatelliteData(satellites, updateSatellite);
+    }
+  }, [satellites, updateSatellite]);
+
   return (
     <div className="satellite-list-page" role="main">
       <SatelliteStatus />
+      <button
+        onClick={handleRandomize}
+        className="randomize-button"
+        aria-label="Случайно изменить координаты спутников"
+      >
+        Случайно изменить координаты спутников
+      </button>
       <SatelliteMap />
       <SatelliteList />
     </div>
   );
 };
 
-export default SatelliteListPage;
+export default React.memo(SatelliteListPage);
